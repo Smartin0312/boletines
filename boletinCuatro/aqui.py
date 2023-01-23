af8dd2436ece438e87020d9d5bf2a68e52e6a7ec
Smartin0312 <115450110+Smartin0312@users.noreply.github.com>
2023-01-23 09:07:07 +0100

    Delete boletinCuatro directory

--- boletinCuatro/aqui.py
+++
@@ -1,9 +0,0 @@
-pago=10
-i=2
-print(f" tiene que pagar en el mes 1,  {pago} euros")
-for i in range (2, 21 ):
-    print(f" tiene que pagar en el mes {i},  {pago*2} euros")
-    pago*=2
-
-print(f" el total es {pago*2-10} euros")
-    
